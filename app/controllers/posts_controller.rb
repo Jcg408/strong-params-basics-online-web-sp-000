b0f8e5c28928ba783ad1f18ba7808a5d8577f0a9
Jessica Gladstone <jcg321@comcast.net>
2019-03-20 15:30:02 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,4 @@
-cclass PostsController < ApplicationController
+class PostsController < ApplicationController
 	def index
 		@posts = Post.all
 	end
@@ -12,18 +12,14 @@ cclass PostsController < ApplicationController
 	end
 
 	def create
-	  @post = Post.new(params.require(:post).permit(:title, :description))
-	  @post.save
-	  redirect_to post_path(@post)
-	end
-
-	def update
-	  @post = Post.find(params[:id])
-	  @post.update(params.require(:post).permit(:title))
-	  redirect_to post_path(@post)
-	end
-
-	def edit
-	  @post = Post.find(params[:id])
-	end
+    @post = Post.new(params.require(:post).permit(:title, :description))
+    @post.save
+    redirect_to post_path(@post)
+  end
+   
+  def update
+    @post = Post.find(params[:id])
+    @post.update(params.require(:post).permit(:title))
+    redirect_to post_path(@post)
+  end
 end
